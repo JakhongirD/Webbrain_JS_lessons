b90cc43ffd5ef044e0d9d5c1604febfb9a49ea24
JakhongirD <abuhafizulloh@gmail.com>
2023-11-12 15:22:27 +0500

    video_task_files

---
+++ script.js
@@ -0,0 +1,28 @@
+// Primitive data types 
+// Number - asosan raqamlar bilan ishlaydi;
+// String - tekst bilan ishlaydi;
+// Boolean - true yoki false ni qaytaradi;
+// Null - bo'sh qiymat;
+// Undefined - hech qanday qiymat yuq;
+// Bigint - katta raqamni ohiriga n qiymati beriladi;
+// Symbol - proyektda faqatgina bir marta beriladi. 
+
+// Non-primitive yoki reference data types:
+// Object - Object, array va function. 
+
+// typeof - qiymatni turini aniqlab beradi.
+
+// let user = {
+//     ism: "Ahmad", status: "Engineer", yosh: 35
+// }
+// console.log(user.ism);
+// console.log(user.status);
+// console.log(user.yosh);
+// console.log(typeof user);
+
+// let arr = [{ism: "Ahmad"}, {status: "Engineer"}, 200, 'Hello, null, undefined']
+// console.log(typeof arr);
+
+// function firstFunction() {
+//     console.log("Hello students!");
+// }
